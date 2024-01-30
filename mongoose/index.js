2ace3276ae293cf1c1df23f25f1ad6672522c1d9
Smriti Ranjan <120196059+smritiranjan65@users.noreply.github.com>
2024-01-30 15:47:19 +0530

    commit

---
+++ mongoose/index.js
@@ -0,0 +1,9 @@
+mongoose = require("mongoose");
+express = require("express");
+
+app = express()
+
+port = 3000
+app.listen(port,() => {console.log(`running : ${port}`)})
+
+mongoose.connect('mongodb://127.0.0.1:27017/aashu').then(() => {console.log('connected successfully')}).catch((error)=>console.log(error));
