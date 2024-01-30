7862f480c94f476734eb3c5d8a30ed14b57e5b97
Smriti Ranjan <120196059+smritiranjan65@users.noreply.github.com>
2024-01-30 15:29:32 +0530

    mongo connection

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 express = require("express");
 items = require("./route/item");
+cookieParser = require('cookie-parser')
 app = express();
 port = 3000;
 
 app.use(express.json())
 app.use(express.urlencoded())
-
+app.use(cookieparser)
 
 app.use('/', items)
 
