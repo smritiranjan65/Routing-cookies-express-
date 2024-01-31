0a3b7b68d552bd9e524fd0ea9c208a85eb5e64bb
Smriti Ranjan <120196059+smritiranjan65@users.noreply.github.com>
2024-01-31 10:14:13 +0530

    Update index.js

--- mongoose/index.js
+++ mongoose/index.js
@@ -7,3 +7,4 @@ port = 3000
 app.listen(port,() => {console.log(`running : ${port}`)})
 
 mongoose.connect('mongodb://127.0.0.1:27017/aashu').then(() => {console.log('connected successfully')}).catch((error)=>console.log(error));
+
